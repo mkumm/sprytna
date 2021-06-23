9fb32ec4d514f313adb4b2da4fb12c20e882f3ec
Mike Kumm <michael@mkumm.com>
2021-06-23 17:09:49 -0400

    updated titles

--- lib/sprytna_web/controllers/page_controller.ex
+++ lib/sprytna_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule SprytnaWeb.PageController do
   use SprytnaWeb, :controller
 
   def services(conn, _params) do
-    render(conn, "index.html", %{page_title: "Services"})
+    render(conn, "index.html", %{page_title: "Custom Programming and Data Services"})
   end
 
   def contact(conn, _params) do
@@ -10,6 +10,6 @@ defmodule SprytnaWeb.PageController do
   end
 
   def about(conn, _params) do
-    render(conn, "about.html", %{page_title: "About"})
+    render(conn, "about.html", %{page_title: "About Craft Development"})
   end
 end
